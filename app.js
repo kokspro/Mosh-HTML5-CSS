39866b3f6ed49d798ee6deccfe9e513b9c5136f9
kokspro <koks.programming@gmail.com>
2023-01-06 14:31:13 -0600

    Loop Practice

---
+++ app.js
@@ -0,0 +1,11 @@
+// let message = "Welcome to the party";
+
+// console.log(message);
+
+// function big( a, b ) {
+//     return ( a > b ) ? a : b;
+// }
+
+function isLandscape( width, height ) {
+    return (width > height);
+}
